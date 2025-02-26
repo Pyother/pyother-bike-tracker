6146993761a2dcacfd6dfb74f5ef6a819897a401
pyother <piotr.dominik.sobol@gmail.com>
2025-02-26 18:21:04 +0100

    Initial commit

---
+++ assets/styles/Layout.js
@@ -0,0 +1,9 @@
+import { StyleSheet } from 'react-native';
+
+const stylesLayout = StyleSheet.create({
+    container: {
+        padding: 20
+    }
+});
+
+export default stylesLayout;
---
+++ assets/styles/Utils.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+
+const stylesUtils = StyleSheet.create({
+    center: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    padding1em: {
+        padding: 20
+    },
+    title: {
+        fontSize: 'large'
+    }
+});
+
+export default stylesUtils;
